6e403444ca9f35bb3620c016b229c5aef49c60bc
Saamer Mansoor <saamer.mansoor@slalom.com>
2018-03-14 20:25:52 -0600

    Google Places API ftw

---
+++ Maps2/Maps2/GoogleResponse.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Maps2
+{
+	public class Location
+	{
+		public double lat { get; set; }
+		public double lng { get; set; }
+	}
+
+	public class Geometry
+	{
+		public Location location { get; set; }
+	}
+
+	public class OpeningHours
+	{
+		public bool open_now { get; set; }
+	}
+
+	public class Photo
+	{
+		public int height { get; set; }
+		public IList<object> html_attributions { get; set; }
+		public string photo_reference { get; set; }
+		public int width { get; set; }
+	}
+
+	public class AltId
+	{
+		public string place_id { get; set; }
+		public string scope { get; set; }
+	}
+
+	public class Result
+	{
+		public Geometry geometry { get; set; }
+		public string icon { get; set; }
+		public string id { get; set; }
+		public string name { get; set; }
+		public OpeningHours opening_hours { get; set; }
+		public IList<Photo> photos { get; set; }
+		public string place_id { get; set; }
+		public string scope { get; set; }
+		public IList<AltId> alt_ids { get; set; }
+		public string reference { get; set; }
+		public IList<string> types { get; set; }
+		public string vicinity { get; set; }
+	}
+
+	public class GoogleResponse
+	{
+		public IList<object> html_attributions { get; set; }
+		public IList<Result> results { get; set; }
+		public string status { get; set; }
+	}
+
+}
--- Maps2/Maps2/PinPage.cs
+++ Maps2/Maps2/PinPage.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace Maps2
 {
@@ -18,23 +20,53 @@ namespace Maps2
 				VerticalOptions = LayoutOptions.FillAndExpand
 			};
 
+			var radius = "5000";
+			var type = "restaurant";
+			var apiKey = "{YOUR API KEY}";
+			var centerLat = 36.9628066;
+			var centerLng = -122.0194722;
 			map.MoveToRegion(MapSpan.FromCenterAndRadius(
-				new Position(36.9628066, -122.0194722), Distance.FromMiles(3))); // Santa Cruz golf course
-
-			var position = new Position(36.9628066, -122.0194722); // Latitude, Longitude
-			var pin = new Pin
+				new Position(centerLat, centerLng), Distance.FromMiles(3))); // Santa Cruz golf course
+			var client = new HttpClient();
+			var uri = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="
+				+ centerLat + "," 
+				+ centerLng + "&radius=" 
+				+ radius + "&type=" 
+				+ type + "&key=" 
+				+ apiKey;
+			// Don't know what the "keyword property does yet
+			string obstring = client.GetStringAsync(uri).Result;
+			var responses = JsonConvert.DeserializeObject<GoogleResponse>(obstring);
+			if (responses.results != null)
 			{
-				Type = PinType.Place,
-				Position = position,
-				Label = "Santa Cruz",
-				Address = "custom detail info"
-			};
-			map.Pins.Add(pin);
+				foreach (var response in responses.results)
+				{
+					var position = new Position(response.geometry.location.lat, response.geometry.location.lng); // Latitude, Longitude
+					var pin = new Pin
+					{
+						Type = PinType.Place,
+						Position = position,
+						Label = response.name,
+						Address = response.vicinity
+					};
+					map.Pins.Add(pin);
+				}
+			}
+			//var position = new Position(36.9628066, -122.0194722); // Latitude, Longitude
+			//var pin = new Pin
+			//{
+			//	Type = PinType.Place,
+			//	Position = position,
+			//	Label = "Santa Cruz",
+			//	Address = "custom detail info"
+			//};
+			//map.Pins.Add(pin);
 
 
 			// create buttons
 			var morePins = new Button { Text = "Add more pins" };
-			morePins.Clicked += (sender, e) => {
+			morePins.Clicked += (sender, e) =>
+			{
 				map.Pins.Add(new Pin
 				{
 					Position = new Position(36.9641949, -122.0177232),
@@ -50,7 +82,8 @@ namespace Maps2
 
 			};
 			var reLocate = new Button { Text = "Re-center" };
-			reLocate.Clicked += (sender, e) => {
+			reLocate.Clicked += (sender, e) =>
+			{
 				map.MoveToRegion(MapSpan.FromCenterAndRadius(
 					new Position(36.9628066, -122.0194722), Distance.FromMiles(3)));
 			};
